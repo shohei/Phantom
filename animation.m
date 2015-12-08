f01b7a10566e253f8514c592a36435042eb9b3d0
Shohei Aoki <shoaok@gmail.com>
2015-12-08 18:38:48 +0900

    複雑な動きのテスト

--- animation.m
+++ animation.m
@@ -10,7 +10,10 @@ theta = pi/12; % rotation around Y axis
 psi = pi/16; % rotation around Z axis
 
 for i_time=1:n_time
+    P = [0.1-i_time/200,0.2+i_time/200,1.1]; % Position Vector of the end effector    
     phi = pi/12*(i_time/10); % rotation around X axis
+    theta = pi/12*(i_time/10); % rotation around Y axis
+    psi = pi/16*(i_time/10); % rotation around Z axis  
     main(P,phi,theta,psi);    
     drawnow;
 end
