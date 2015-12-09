3b2966a7e58aa81532b9a93adfc93ae065cab67c
Shohei Aoki <shoaok@gmail.com>
2015-12-09 10:53:48 +0900

    テーブル高さPzの編集UIを追加

--- dashboard.m
+++ dashboard.m
@@ -22,7 +22,7 @@ function varargout = dashboard(varargin)
 
 % Edit the above text to modify the response to help dashboard
 
-% Last Modified by GUIDE v2.5 08-Dec-2015 22:05:49
+% Last Modified by GUIDE v2.5 09-Dec-2015 10:38:53
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -59,25 +59,26 @@ handles.D = 0.3; % Distance between 2 slider of the pair
 handles.lc = 1.1; % Length of rod
 handles.rb = 1; % Distance between origin and actuator on X-Y plane
 handles.re = 0.4; % Radius of table
+handles.Pz = 1.1;
 
 % initialize EditText
 set(handles.edit_D,'String',num2str(handles.D));
 set(handles.edit_lc,'String',num2str(handles.lc));
 set(handles.edit_rb,'String',num2str(handles.rb));
 set(handles.edit_re,'String',num2str(handles.re));
+set(handles.edit_Pz,'String',num2str(handles.Pz));
 
 set(handles.slider_D,'Value',handles.D);
 set(handles.slider_lc,'Value',handles.lc);
 set(handles.slider_rb,'Value',handles.rb);
 set(handles.slider_re,'Value',handles.re);
+set(handles.slider_Pz,'Value',handles.Pz);
 
 % Update handles structure
 guidata(hObject, handles);
 
 % UIWAIT makes dashboard wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
-
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = dashboard_OutputFcn(hObject, eventdata, handles)
@@ -89,7 +90,6 @@ function varargout = dashboard_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
 function edit_D_Callback(hObject, eventdata, handles)
 % hObject    handle to edit_D (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -97,6 +97,9 @@ function edit_D_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit_D as text
 %        str2double(get(hObject,'String')) returns contents of edit_D as a double
+set(handles.slider_D,'Value',str2double(get(hObject,'String')));
+guidata(hObject,handles);
+
 
 % --- Executes during object creation, after setting all properties.
 function edit_D_CreateFcn(hObject, eventdata, handles)
@@ -119,6 +122,9 @@ function edit_lc_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit_lc as text
 %        str2double(get(hObject,'String')) returns contents of edit_lc as a double
+set(handles.slider_lc,'Value',str2double(get(hObject,'String')));
+guidata(hObject,handles);
+
 
 % --- Executes during object creation, after setting all properties.
 function edit_lc_CreateFcn(hObject, eventdata, handles)
@@ -139,6 +145,8 @@ function edit_rb_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit_rb as text
 %        str2double(get(hObject,'String')) returns contents of edit_rb as a double
+set(handles.slider_rb,'Value',str2double(get(hObject,'String')));
+guidata(hObject,handles);
 
 % --- Executes during object creation, after setting all properties.
 function edit_rb_CreateFcn(hObject, eventdata, handles)
@@ -159,6 +167,8 @@ function edit_re_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit_re as text
 %        str2double(get(hObject,'String')) returns contents of edit_re as a double
+set(handles.slider_re,'Value',str2double(get(hObject,'String')));
+guidata(hObject,handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -172,114 +182,6 @@ function edit_re_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-% --- Executes on button press in pushbutton1.
-function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% P = [0.1,0.2,1.1]; % Position Vector of the end effector
-% phi = pi/12; % rotation around X axis
-% theta = pi/12; % rotation around Y axis
-% psi = pi/16; % rotation around Z axis
-% main(P,phi,theta,psi);
-
-% D=handles.slider_D.Value;
-% lc=handles.slider_lc.Value;
-% rb=handles.slider_rb.Value;
-% re=handles.slider_re.Value;
-D=str2double(handles.edit_D.String);
-lc=str2double(handles.edit_lc.String);
-rb=str2double(handles.edit_rb.String);
-re=str2double(handles.edit_re.String);
-check=handles.checkbox_debug;
-
-n_time = 20;
-
-clearDebugMessage();
-for i_time=1:n_time
-    P = [0.1-i_time/200,0.2+i_time/200,1.1]; % Position Vector of the end effector
-    phi = pi/12*(i_time/10); % rotation around X axis
-    theta = pi/12*(i_time/10); % rotation around Y axis
-    psi = pi/16*(i_time/10); % rotation around Z axis
-    main(D,lc,rb,re,P,phi,theta,psi);
-    if check==1
-        showDebugMessage(i_time,phi,theta,psi);
-    end
-    drawnow;
-end
-
-function clearDebugMessage()
-uicontrol('Style', 'text',...
-    'String', '',... 
-    'Units','normalized',...
-    'FontSize', 10,....
-    'FontName','OCR A Std',...
-    'HorizontalAlignment','left',...
-    'Max',5,...
-    'Position', [0.02 0.85 0.2 0.15]);
-
-uicontrol('Style', 'text',...
-    'String', '',... 
-    'Units','normalized',...
-    'FontSize', 10,....
-    'FontName','OCR A Std',...
-    'HorizontalAlignment','left',...
-    'Max',5,...
-    'Position', [0.4 0.85 0.2 0.15]);
-
-
-function showDebugMessage(i_time,phi,theta,psi)
-msg = sprintf(strcat('%d frame elapsed\n',...
-    'x-angle: %.2f.[rad]\n',...
-    'y-angle: %.2f.[rad]\n',...
-    'z-angle: %.2f.[rad]\n'),i_time,phi,theta,psi);
-
-msg2 = sprintf(strcat('%d frame elapsed\n',...
-    'x-angle: %.2f.[rad]\n',...
-    'y-angle: %.2f.[rad]\n',...
-    'z-angle: %.2f.[rad]\n'),i_time,phi,theta,psi);
-
-uicontrol('Style', 'text',...
-    'String', msg,... 
-    'Units','normalized',...
-    'FontSize', 10,....
-    'FontName','OCR A Std',...
-    'HorizontalAlignment','left',...
-    'Max',5,...
-    'Position', [0.02 0.85 0.2 0.15]);
-
-uicontrol('Style', 'text',...
-    'String', msg2,... 
-    'Units','normalized',...
-    'FontSize', 10,....
-    'FontName','OCR A Std',...
-    'HorizontalAlignment','left',...
-    'Max',5,...
-    'Position', [0.4 0.85 0.2 0.15]);
-
 
 % --- Executes on slider movement.
 function slider_D_Callback(hObject, eventdata, handles)
@@ -350,6 +252,29 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
+function edit_Pz_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_Pz (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_Pz as text
+%        str2double(get(hObject,'String')) returns contents of edit_Pz as a double
+set(handles.slider_Pz,'Value',str2double(get(hObject,'String')));
+guidata(hObject,handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_Pz_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_Pz (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
 % --- Executes on slider movement.
 function slider_re_Callback(hObject, eventdata, handles)
 % hObject    handle to slider_re (see GCBO)
@@ -372,9 +297,31 @@ function slider_re_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
-handles.checkbox_debug = get(hObject,'Value');
+
+
+% --- Executes on slider movement.
+function slider_Pz_Callback(hObject, eventdata, handles)
+% hObject    handle to slider_Pz (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+handles.slider_Pz = get(hObject,'Value');
+set(handles.edit_Pz,'String',num2str(handles.slider_Pz));
 guidata(hObject,handles);
 
+
+% --- Executes during object creation, after setting all properties.
+function slider_Pz_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider_Pz (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
 
 % --- Executes on button press in checkbox_debug.
 function checkbox_debug_Callback(hObject, eventdata, handles)
@@ -385,3 +332,114 @@ function checkbox_debug_Callback(hObject, eventdata, handles)
 % Hint: get(hObject,'Value') returns toggle state of checkbox_debug
 handles.checkbox_debug = get(hObject,'Value');
 guidata(hObject,handles);
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% P = [0.1,0.2,1.1]; % Position Vector of the end effector
+% phi = pi/12; % rotation around X axis
+% theta = pi/12; % rotation around Y axis
+% psi = pi/16; % rotation around Z axis
+% main(P,phi,theta,psi);
+
+% D=handles.slider_D.Value;
+% lc=handles.slider_lc.Value;
+% rb=handles.slider_rb.Value;
+% re=handles.slider_re.Value;
+D=str2double(handles.edit_D.String);
+lc=str2double(handles.edit_lc.String);
+rb=str2double(handles.edit_rb.String);
+re=str2double(handles.edit_re.String);
+Pz=str2double(handles.edit_Pz.String);
+
+check=handles.checkbox_debug;
+
+n_time = 20;
+
+clearDebugMessage();
+for i_time=1:n_time
+    P = [0.1-i_time/200,0.2+i_time/200,Pz]; % Position Vector of the end effector
+    phi = pi/12*(i_time/10); % rotation around X axis
+    theta = pi/12*(i_time/10); % rotation around Y axis
+    psi = pi/16*(i_time/10); % rotation around Z axis
+    main(D,lc,rb,re,P,phi,theta,psi);
+    if check==1
+        showDebugMessage(i_time,phi,theta,psi);
+    end
+    drawnow;
+end
+
+function clearDebugMessage()
+uicontrol('Style', 'text',...
+    'String', '',... 
+    'Units','normalized',...
+    'FontSize', 10,....
+    'FontName','OCR A Std',...
+    'HorizontalAlignment','left',...
+    'Max',5,...
+    'Position', [0.02 0.85 0.2 0.15]);
+
+uicontrol('Style', 'text',...
+    'String', '',... 
+    'Units','normalized',...
+    'FontSize', 10,....
+    'FontName','OCR A Std',...
+    'HorizontalAlignment','left',...
+    'Max',5,...
+    'Position', [0.4 0.85 0.2 0.15]);
+
+
+function showDebugMessage(i_time,phi,theta,psi)
+msg = sprintf(strcat('%d frame elapsed\n',...
+    'x-angle: %.2f.[rad]\n',...
+    'y-angle: %.2f.[rad]\n',...
+    'z-angle: %.2f.[rad]\n'),i_time,phi,theta,psi);
+
+msg2 = sprintf(strcat('%d frame elapsed\n',...
+    'x-angle: %.2f.[rad]\n',...
+    'y-angle: %.2f.[rad]\n',...
+    'z-angle: %.2f.[rad]\n'),i_time,phi,theta,psi);
+
+uicontrol('Style', 'text',...
+    'String', msg,... 
+    'Units','normalized',...
+    'FontSize', 10,....
+    'FontName','OCR A Std',...
+    'HorizontalAlignment','left',...
+    'Max',5,...
+    'Position', [0.02 0.85 0.2 0.15]);
+
+uicontrol('Style', 'text',...
+    'String', msg2,... 
+    'Units','normalized',...
+    'FontSize', 10,....
+    'FontName','OCR A Std',...
+    'HorizontalAlignment','left',...
+    'Max',5,...
+    'Position', [0.4 0.85 0.2 0.15]);
